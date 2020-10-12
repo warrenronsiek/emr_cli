e68a3bab572c7ca3290fb54dec597ee98f9e6cda
warrenronsiek <warren@warren-r.com>
2020-10-12 17:26:11 +0200

    more tests

--- test/emr_cli/utils_test.clj
+++ test/emr_cli/utils_test.clj
@@ -4,16 +4,14 @@
             [emr-cli.utils :refer [parse-conf]]))
 
 
-(deftest conf-consumption
+(deftest good-conf-consumption
   (testing "configs validate"
-    (let [good-conf (parse-conf (slurp (io/resource "example_conf.yml")))
-          bad-conf (slurp (io/resource "bad_conf.yml"))
-          not-even-wrong-conf "asdf"]
+    (let [good-conf (parse-conf (slurp (io/resource "example_conf.yml")))]
       (is (= (:name good-conf) "test"))
       (is (= (:logUri good-conf) "s3://spark-boilerplate/logs/"))
       (is (= (:subnet good-conf) "subnet-b0711ec9"))
       (is (= (:instanceType good-conf) "m4.xlarge"))
-      (is (= (:key good-conf) "warren-laptop") )
+      (is (= (:key good-conf) "warren-laptop"))
       (is (= (:instanceCount good-conf) 1))
       (is (= (:bidPct good-conf) 50))
       (is (= (:serviceRole good-conf) "emr-default-role"))
@@ -21,3 +19,12 @@
       (is (= (:region good-conf) "us-east-1"))
       (is (= (:tags good-conf) [{:Key "Testkey1" :Value "TestValue1"}
                                 {:Key "Testkey2" :Value "TestValue2"}])))))
+
+(deftest bad-conf-consumption
+  (testing "bad conf validation"
+    (let [bad-conf (slurp (io/resource "bad_conf.yml"))]
+      (is (thrown-with-msg? java.lang.Exception #"config validation failed" (parse-conf bad-conf))))))
+
+(deftest crazy-conf-consumption
+  (testing "not even wrong conf"
+    (is (thrown? java.lang.ClassCastException (parse-conf "asdf")))))
