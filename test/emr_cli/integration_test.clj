115823254e8251271c7db9221325f958eb71bcbe
wronsiek <warrenronsiek@gmail.com>
2020-10-18 17:36:45 +0200

    started writing test

--- test/emr_cli/integration_test.clj
+++ test/emr_cli/integration_test.clj
@@ -1,7 +1,13 @@
 (ns emr-cli.integration-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [emr-cli.utils :refer [parse-conf client-builder]]))
 
+(def ^:dynamic *cluster-id*)
+(def emr-client (client-builder (parse-conf ("example_conf.yml")) "emr"))
+
+(def cluster-fixture [test-fn]
+  ())
 
 (deftest creates-cluster
   "create cluster works"
-  )
+  (testing "cluster creation"))
